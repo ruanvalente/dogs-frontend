65caca301d393e351bbfdf6045ca34505c7b2c19
ruanvalente <ruanopensuselinux@gmail.com>
2022-12-19 23:23:18 -0300

    fix: fix svg import

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -5,12 +5,14 @@ import { UserStorage } from '@/context/userContext'
 import { AccountPage } from '@/pages/Account/AccountPage'
 import { Home } from '@/pages/Home/Home'
 import { Login } from '@/pages/Login/routes'
+import { Header } from '@/core-ui/Header'
 
 export function Router() {
   return (
     <>
       <BrowserRouter>
         <UserStorage>
+          <Header />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }]
   }
